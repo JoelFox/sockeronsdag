6390633ad5a1f20369ce2c65ea37c922705d3d21
Joel Fox Apelgren <joel.apelgren@gmail.com>
2021-11-11 20:29:44 +0100

    Create react app

---
+++ sockeronsdag/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import polkagris from './polkagris.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={polkagris} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
