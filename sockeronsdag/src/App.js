b860c750fad4288fb3f315d41e0c4fdec8a3ec5d
Joel Fox Apelgren <joel.apelgren@gmail.com>
2021-11-11 20:47:35 +0100

    Use sweeter font

--- sockeronsdag/src/App.js
+++ sockeronsdag/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Sockeronsdag</h1>
         <img src={polkagris} className="App-logo" alt="logo" />
       </header>
     </div>
