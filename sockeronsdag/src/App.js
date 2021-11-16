d76100a1246ddb9459dba2b901c148aa19e7a2a2
Joel Pettersson <joel.pettersson@makingwaves.com>
2021-11-16 16:42:30 +0100

    events added

--- sockeronsdag/src/App.js
+++ sockeronsdag/src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import polkagris from './polkagris.png';
+import sugarprovider from './Hemkop_logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sockeronsdag</h1>
-        <img src={polkagris} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-headingH1">Sockeronsdag</h1>
+				<h4 className="App-subHeading">Upcoming event:</h4>
+				<div className="App-event">
+					<ul className="App-eventList">
+						<li className="App-eventListItem is-animated">
+							<span>2021-11-17</span><img src={sugarprovider} className="App-eventImage" />
+						</li>
+						<li className="App-eventListItem">
+							<span>2021-11-24</span><img src={sugarprovider} className="App-eventImage" />
+						</li>
+					</ul>
+				</div>
+				<img src={polkagris} className="App-logo" alt="logo" />
+			</header>
+		</div>
+	);
 }
 
 export default App;
