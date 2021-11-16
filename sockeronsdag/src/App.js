3bd014c5e431e21e0212e2517afab51dd65990f2
Joel Pettersson <joel.pettersson@makingwaves.com>
2021-11-16 17:39:47 +0100

    clock added

--- sockeronsdag/src/App.js
+++ sockeronsdag/src/App.js
@@ -1,8 +1,25 @@
 import polkagris from './polkagris.png';
+import AnalogClock from 'analog-clock-react';
 import sugarprovider from './Hemkop_logo.png';
 import './App.css';
 
 function App() {
+
+	let options = {
+		width: "300px",
+		border: true,
+		borderColor: "#2e2e2e",
+		baseColor: "#17a2b8",
+		centerColor: "#459cff",
+		centerBorderColor: "#ffffff",
+		handColors: {
+			second: "#d81c7a",
+			minute: "#ffffff",
+			hour: "#ffffff"
+		}
+	};
+	
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -19,9 +36,11 @@ function App() {
 					</ul>
 				</div>
 				<img src={polkagris} className="App-logo" alt="logo" />
+				<AnalogClock {...options} />
 			</header>
 		</div>
 	);
+
 }
 
 export default App;
