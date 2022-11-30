8e254a0b3489e4c7cb7e421ca2bab0d28f9d2ea4
Joel Fox Apelgren <joel.apelgren@makingwaves.com>
2022-11-30 14:29:39 +0100

    Update dates

--- sockeronsdag/src/App.js
+++ sockeronsdag/src/App.js
@@ -27,13 +27,16 @@ function App() {
 				<div className="App-event">
 					<ul className="App-eventList">
 						<li className="App-eventListItem is-animated">
-							<span>2022-11-23</span><img src={sugarprovider} className="App-eventImage" />
-						</li>
-						<li className="App-eventListItem">
 							<span>2022-11-30</span><img src={sugarprovider} className="App-eventImage" />
 						</li>
 						<li className="App-eventListItem">
 							<span>2022-12-07</span><img src={sugarprovider} className="App-eventImage" />
+						</li>
+						<li className="App-eventListItem">
+							<span>2022-12-14</span><img src={sugarprovider} className="App-eventImage" />
+						</li>
+						<li className="App-eventListItem">
+							<span>2022-12-21</span><img src={sugarprovider} className="App-eventImage" />
 						</li>
 					</ul>
 				</div>
